3dab6cd7e4f080bda2f8e2884b63c2543d1c887f
Ubuntu <ubuntu@ip-172-31-30-204.eu-west-3.compute.internal>
2021-08-08 15:07:21 +0000

    odoo deletion

--- aws_odoo_server3.sh
+++ aws_odoo_server3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 ./odoo-14.0/odoo-bin -r firm01 -w firm01 --addons-path=./odoo-14.0/addons,./src/custom -d firm01 --db_host odoo-db.cnfvu3hjlwx5.eu-west-3.rds.amazonaws.com --db_port 5432
+python3 ./odoo/odoo-bin -r firm01 -w firm01 --addons-path=./odoo/addons -d firm01 --db_host odoo-db.cnfvu3hjlwx5.eu-west-3.rds.amazonaws.com --db_port 5432
