22bd2e470ab59ea31a9764260b551360455019d8
Ubuntu <ubuntu@ip-172-31-47-133.eu-west-3.compute.internal>
2021-08-10 21:51:25 +0000

    simple update

--- aws_odoo_server3.sh
+++ aws_odoo_server3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 ~/odoo/odoo-bin -r firm01 -w firm01 --addons-path=~/odoo/addons,~/TFM_MDE/src/custom -d firm01 --db_host odoo-db.cnfvu3hjlwx5.eu-west-3.rds.amazonaws.com --db_port 5432
+python3 ~/odoo/odoo-bin -r odoo -w odoo --addons-path=~/odoo/addons,~/TFM_MDE/src/custom -d odoo --db_host odoo-db.cnfvu3hjlwx5.eu-west-3.rds.amazonaws.com --db_port 5432 --without-demo=WITHOUT_DEMO
