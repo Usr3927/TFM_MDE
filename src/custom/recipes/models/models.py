645bfcebbd864f52f1540f4eca3ada262f59d91d
Eduard Moreno Triguero <usr3927@gmail.com>
2021-08-18 23:34:53 +0200

    ingredientes

--- src/custom/recipes/models/models.py
+++ src/custom/recipes/models/models.py
@@ -6,26 +6,37 @@ from odoo.exceptions import ValidationError
 
 class ProductTemplate(models.Model):
   _inherit = 'product.template'
-  ingredient_id = fields.Many2many('product.template', relation='template_recipe_rel' ,column1="template_id", column2="ingredient_id", string='Ingredientes', help="Lista de ingredientes.")
-  ingredient_ids = fields.Many2many('product.template', relation='template_recipe_rel' , column1="ingredient_id", column2="template_id" ,string='Platos')
-  @api.constrains('ingredient_id')
+  ingredient_id = fields.One2many('recipes.ingredient', inverse_name="meal_id", string='Ingrediente')
+    
+#  ingredient_id = fields.Many2many('product.template', relation='template_recipe_rel' ,column1="template_id", column2="ingredient_id", string='Ingredientes', help="Lista de ingredientes.")
+#  ingredient_ids = fields.Many2many('product.template', relation='template_recipe_rel' , column1="ingredient_id", column2="template_id" ,string='Platos')
+#  @api.constrains('ingredient_id')
+#  def _check_adding_itself(self):
+#    for ingrdnt in self.ingredient_id:
+#      if ingrdnt.id == self.id:
+#        raise ValidationError('No es posible crear recetas recursivas.')
+
+class Ingredient(models.Model):
+  _name = 'recipes.ingredient'
+  _description = 'Ingredientes por receta.'
+  name=fields.Char(string='Ingrediente')
+  meal_id = fields.Many2one("product.template", string='Plato')
+  raw_id = fields.Many2many('product.template', relation='template_recipe_rel', column1="template_id", column2="ingredient_id", string="Materia prima")
+  description = fields.Text(string='Descripción')
+  grams=fields.Float(string='Gramos')
+  litres=fields.Float(string='Litros')
+  
+  _sql_constraints = [
+    ('ingredient_name_unique', 'unique(name)', 'El ingrediente ya existe!')
+]
+
+  @api.constrains('meal_id')
   def _check_adding_itself(self):
-    for ingrdnt in self.ingredient_id:
-      if ingrdnt.id == self.id:
-        raise ValidationError('No es posible crear recetas recursivas.')
-#    for ingrdnts in self.ingredient_id:
-#      raise ValidationError(ingrdnts.name)
-#class recipes(models.Model):
-#  _name = 'recipes.ingredients'
-#  _description = 'Ingredientes por receta.'
+    parent_id = self.env.context.get('parent_id') 
+    parent_model = self.env.context.get('parent_model')  
+#    raise ValidationError(parent_model)     
 
-#  ingredient = fields.Integer(string="Id. ingrediente", default=lambda self: self.env['ir.sequence'].next_by_code('increment_your_field'))
-#  meal_id = fields.Many2one("product.template", string="Producto")
-#  description = fields.Text(string='Descripción')
-#  grams = fields.Float(string='Gramos')
-#  litres = fields.Float(string='Litros')
-
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+#  @api.depends('value')
+#  def _value_pc(self):
+#   for record in self:
+#     record.value2 = float(record.value) / 100
