70b42a7986cfe2fadc44dcf4e8a5718856e5fcb7
Eduard Moreno Triguero <usr3927@gmail.com>
2021-08-19 02:00:37 +0200

    ingredients update

--- src/custom/recipes/models/models.py
+++ src/custom/recipes/models/models.py
@@ -7,15 +7,14 @@ from odoo.exceptions import ValidationError
 class ProductTemplate(models.Model):
   _inherit = 'product.template'
   ingredient_id = fields.One2many('recipes.ingredient', inverse_name="meal_id", string='Ingrediente')
+  
+  @api.constrains('ingredient_id')
+  def _check_adding_itself(self):
+    for ingrdnt in self.ingredient_id:
+      for prdct in ingrdnt.raw_id:
+        if self.id == prdct.id :
+          raise ValidationError('Los ingredientes del plato deben ser diferentes del propio plato. En el ingrediente "'+ingrdnt.name+'" se ha incluido el plato "'+prdct.name+'".')
     
-#  ingredient_id = fields.Many2many('product.template', relation='template_recipe_rel' ,column1="template_id", column2="ingredient_id", string='Ingredientes', help="Lista de ingredientes.")
-#  ingredient_ids = fields.Many2many('product.template', relation='template_recipe_rel' , column1="ingredient_id", column2="template_id" ,string='Platos')
-#  @api.constrains('ingredient_id')
-#  def _check_adding_itself(self):
-#    for ingrdnt in self.ingredient_id:
-#      if ingrdnt.id == self.id:
-#        raise ValidationError('No es posible crear recetas recursivas.')
-
 class Ingredient(models.Model):
   _name = 'recipes.ingredient'
   _description = 'Ingredientes por receta.'
@@ -25,16 +24,7 @@ class Ingredient(models.Model):
   description = fields.Text(string='Descripción')
   grams=fields.Float(string='Gramos')
   litres=fields.Float(string='Litros')
-  
-  _sql_constraints = [
-    ('ingredient_name_unique', 'unique(name)', 'El ingrediente ya existe!')
-]
 
-  @api.constrains('meal_id')
-  def _check_adding_itself(self):
-    parent_id = self.env.context.get('parent_id') 
-    parent_model = self.env.context.get('parent_model')  
-#    raise ValidationError(parent_model)     
 
 #  @api.depends('value')
 #  def _value_pc(self):
