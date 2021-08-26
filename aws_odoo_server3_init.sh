db0d3f752f2af43d9b5e9d8ac532dfc2cc93618e
Ubuntu <ubuntu@ip-172-31-12-91.eu-west-3.compute.internal>
2021-08-26 13:54:54 +0000

    update scripts

--- aws_odoo_server3_init.sh
+++ aws_odoo_server3_init.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 ~/odoo/odoo-bin -i base -r odoo -w odoo --addons-path=~/odoo/addons,~/TFM_MDE/src/custom -d odoo --db_host odoo-db.cnfvu3hjlwx5.eu-west-3.rds.amazonaws.com --db_port 5432 --without-demo=WITHOUT_DEMO
+python3 ~/odoo/odoo-bin -i base -r odoo -w odoo --addons-path=~/odoo/addons,~/TFM_MDE/src/custom -d odoo --db_host localhost --db_port 5432 --without-demo=WITHOUT_DEMO
