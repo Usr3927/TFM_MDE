ccfca619ffcbcfc1f2e03599e73bbb6ff8e2608c
Eduard Moreno <usr3927@gmail.com>
2021-09-24 18:50:41 +0200

    cron ok

--- src/custom/etl_launcher/models/models.py
+++ src/custom/etl_launcher/models/models.py
@@ -3,15 +3,9 @@
 from odoo import models, fields, api
 from odoo.addons.etl_launcher.ETL.ETL import stg_load
 
-class etl_schedule(models.Model):
-  _name = 'etl_launcher.etl_schedule'
-  _description = 'Lanzador de la ETL'
-  name = fields.Char(string="Nombre")
-  fecha = fields.Datetime(string='Fecha y hora', required=True, readonly=False, default=fields.Datetime.now())
-  description = fields.Text(string='Descripción')
-
 class etl_ondemand(models.Model):
   _name = 'etl_launcher.etl_ondemand'
+  _description = 'ETL launcher'
   def onDemandLoad (self, context=None): 
     print("ETL started")
     stg_load()
