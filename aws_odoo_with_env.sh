981d96f4bc23ff813705080a735dccd91de18b32
Ubuntu <ubuntu@ip-172-31-12-91.eu-west-3.compute.internal>
2021-10-01 23:21:47 +0000

    autostart

--- aws_odoo_with_env.sh
+++ aws_odoo_with_env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /home/TFM_MDE/VirtualEnvrnmnt/odoo/bin/activate
-python3 /home/TFM_MDE/odoo/odoo-bin -r odoo -w odoo --addons-path=~/odoo/addons,~/TFM_MDE/src/custom -d odoo --db_host localhost --db_port 5432 --without-demo=WITHOUT_DEMO
+source ~/VirtualEnvrnmnt/odoo/bin/activate
+python3 ~/odoo/odoo-bin -r odoo -w odoo --addons-path=~/odoo/addons,~/TFM_MDE/src/custom -d odoo --db_host localhost --db_port 5432 --without-demo=WITHOUT_DEMO
